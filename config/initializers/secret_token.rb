21e58d65581bbbc7a2990785d2cfe2cd97b580dd
andreea <andreea@GX1573.local>
2013-11-20 13:07:36 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cap2Test::Application.config.secret_token = 'cb802ad17c1554ab6fe70a798d8f655a3ede58a2a95bb73e1e2184708648a699b6433f514688d653f1a88e04cef25d97fcfc74bd8c037d1014ecf6fea617077f'
